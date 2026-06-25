ab72b2e7641999e94d0f7fca5f32fa3b61c5bde0
Ian Misael Reis <ianmisael@gmail.com>
2026-06-25 17:01:33 -0300

    Ajustar health check e hosts para deploy AWS

--- config/settings.py
+++ config/settings.py
@@ -33,8 +33,7 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # Application definition
 
